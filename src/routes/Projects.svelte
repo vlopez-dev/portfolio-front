f2fe498f1cdd4296e5ae7b4986f583d9d76cdd93
Victor <victorl_222@hotmail.com>
2022-09-28 18:00:44 -0300

    get data from rest api

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -1,4 +1,18 @@
 <script>
+  import { onMount } from "svelte";
+  const projects=[]
+
+  onMount(async () => {
+  fetch("http://127.0.0.1:8000/project/")
+  .then(response => response.json())
+  .then(data => {
+    data = projects
+		console.log(data);
+  }).catch(error => {
+    console.log(error);
+    return [];
+  });
+});
 
 </script>
 
@@ -13,6 +27,9 @@
         <figure class="image is-4by3">
           <img src="https://images.unsplash.com/photo-1660308985375-bf5984ff786e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1121&q=80" alt="Placeholder image">
         </figure>
+        <!-- {#each projects as poject}
+          <p style="color:black">{project.name}</p>
+        {/each} -->
       </div>
       
     </div>
