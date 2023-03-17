762eaa2b5134936dcfa5b2a08c166fa2d25e15d0
victor <victorl_222@hotmail.com>
2023-03-17 16:31:05 -0300

    change captcha front

--- frontend/src/routes/Contact.svelte
+++ frontend/src/routes/Contact.svelte
@@ -102,19 +102,11 @@ function onRecaptcha(response) {
             </span>
               Send</button>
           </div>
-<!-- 
-          <div class="field">
-            <label class="label custom-label">reCAPTCHA</label>
-            <div class="control">
-              <div class="g-recaptcha" data-sitekey="TU_SITE_KEY" data-callback="onRecaptcha"></div>
-              <input type="hidden" name="recaptcha_response" bind:value={recaptchaResponse} />
-            </div>
-          </div> -->
 
         </div>
 
       </form>
-      <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
+      <div class="g-recaptcha" data-sitekey="6Ld7NskkAAAAAJKzdpVtsTXKBUdX_TT3dizQHxA8"></div>
 
       <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
