0377a73f4c7009ea509f4cfcd22dc37f6227c453
victor <victorl_222@hotmail.com>
2023-03-27 17:29:26 -0300

    test carousel svelte

--- frontend/src/routes/Carrusel.svelte
+++ frontend/src/routes/Carrusel.svelte
@@ -1,35 +1,11 @@
 <script>
   import Carousel from 'svelte-carousel';
   import { onMount } from "svelte";
-
-let options = {
-  autoplay: true,
-  autoplaySpeed: 3000,
-  arrows: false,
-  dots: true,
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
+  import { slide } from "svelte/transition";
 
   let certificates = [];
-
+  let currentSlide = 0;
+  
   onMount(async () => {
     fetch("http://127.0.0.1:8001/certificates/")
       .then((response) => response.json())
@@ -44,27 +20,42 @@ let options = {
       });
   });
 
-  let loaded = [];
-
-function handleLoad(index) {
-  loaded = [...loaded, index];
-}
+  const nextImage = () => {
+    currentSlide += 1;
+  };
 
 
+  const prevImage = () => {
+    currentSlide -= 1;
+  };
 
 
 
 
 </script>
-<Carousel let:loaded {options}>
-  {#each certificates as certificate, certificateIndex (certificate)}
-    <div class="img-container" >
-      {#if loaded.includes(certificateIndex)}
-        <img src="{certificate.imagecert}" alt="Certificado" on:load={(event) => handleLoad(certificateIndex)} />
-      {/if}
-    </div>
-  {/each}
+
+<Carousel
+let:loaded
+>
+  {#if certificates.length === 0}
+    <div>No images to show</div>
+  {:else}
+    {#each certificates as certificate}
+      <img
+        transition:slide
+        src={certificate.imagecert}
+        alt={certificate.title}
+        width={400}
+        height={300}
+      />
+    {/each}
+  {/if}
 </Carousel>
+
+
+
+
+
 <style>
 
 </style>
