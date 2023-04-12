c91e92e25d46e619ab75ac88cd8a16fdcde7b7aa
victor <victorl_222@hotmail.com>
2023-04-12 14:15:52 -0300

    add debug variable

--- backend/portafolio/portafolio/settings.py
+++ backend/portafolio/portafolio/settings.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
+DEBUG = os.getenv('DEBUG')
+
 
 # Obtiene el valor de la variable de entorno DATABASE_URL
 DB_PORTA_NAME = os.getenv('DB_PORTA_NAME')
@@ -58,19 +60,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jienf(j#njp@^*^()5lzp6a4%+243^zp#rky&eu9ak3lnd6+^9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['vic.uy','www.vic.uy','127.0.0.1']
 load_dotenv()
 
-
-EMAIL_BACKEND=os.getenv('EMAIL_BACKEND')
-EMAIL_HOST=os.getenv('EMAIL_HOST')
-EMAIL_PORT=os.getenv('EMAIL_PORT')
-EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS=os.getenv('EMAIL_USE_TLS')
-EMAIL_USE_SSL=os.getenv('EMAIL_USE_SSL')
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
