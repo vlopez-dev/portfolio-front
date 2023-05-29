060d505536a1dd04de2c3ee1769e782536272ff5
victor <victorl_222@hotmail.com>
2023-05-29 10:50:16 -0300

    add static files url

--- backend/portafolio/portafolio/settings.py
+++ backend/portafolio/portafolio/settings.py
@@ -175,7 +175,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL ='/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
