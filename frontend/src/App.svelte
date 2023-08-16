3105a57fa7a791c0ada4a01f538fb14d5336234e
victor <victorl_222@hotmail.com>
2023-08-16 14:20:56 -0300

    test matomo

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -7,6 +7,21 @@
   import Router from "svelte-spa-router"
   import Footer from "./routes/Footer.svelte";
     
+
+
+
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://vicuy.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='//cdn.matomo.cloud/vicuy.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+
   let routes={
     "/":Hero,
     "/about":About,
