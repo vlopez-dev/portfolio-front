55cd9f54a365eeacef0ac286a9fb576602c950b6
Victor López <victor.lopez.r@estudiantes.utec.edu.uy>
2025-01-03 09:43:35 -0300

    add secretkey

--- backend/portafolio/portafolio/settings.py
+++ backend/portafolio/portafolio/settings.py
@@ -57,10 +57,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jienf(j#njp@^*^()5lzp6a4%+243^zp#rky&eu9ak3lnd6+^9'
+SECRET_KEY = '7y+b=2!pm+cp_-hgr83ebzec@fa5g9d_n3tl4oxbh7=&o$58q+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['vic.uy','www.vic.uy','127.0.0.1']
 load_dotenv()
@@ -118,27 +118,27 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
-
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME":DB_PORTA_NAME,
-#         "USER":DB_PORTA_USER,
-#         "PASSWORD":DB_PORTA_PASSWORD,
-#         "HOST":DB_HOST,
-#         "PORT":DB_PORT,
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME":DB_PORTA_NAME,
+        "USER":DB_PORTA_USER,
+        "PASSWORD":DB_PORTA_PASSWORD,
+        "HOST":DB_HOST,
+        "PORT":DB_PORT,
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
