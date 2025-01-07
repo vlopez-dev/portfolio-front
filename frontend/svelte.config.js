fc1d6f01c0f4df921a1130aea0ead0c42fc24510
Victor López <victor.lopez.r@estudiantes.utec.edu.uy>
2025-01-07 14:03:17 -0300

    config svelte.conf and vite.conf

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,13 +1,32 @@
+// import adapter from "@sveltejs/adapter-node";
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+
 import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            out: 'build', // Directorio de salida
+            precompress: false, // Precomprimir archivos
+            env: {
+                host: '0.0.0.0',
+                port: process.env.PORT || 5173
+            }
+        })
+    }
 };
 
 export default config;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,14 @@
+// import { defineConfig } from "vitest/config";
+// import { sveltekit } from '@sveltejs/kit/vite';
+
+// export default defineConfig({
+//     plugins: [sveltekit()],
+
+//     test: {
+//         include: ['src/**/*.{test,spec}.{js,ts}']
+//     }
+// });
+
 import { defineConfig } from "vitest/config";
 import { sveltekit } from '@sveltejs/kit/vite';
 
