83e004cbe9d1a8a2a0a782ad20685b137544fd39
victor lopez <victorl_222@hotmail.com>
2025-01-17 14:17:19 -0300

    project link

--- frontend/src/lib/Projects.svelte
+++ frontend/src/lib/Projects.svelte
@@ -12,7 +12,7 @@
 
 
   onMount(async () => {
-    fetch("http://127.0.0.1:8000/project/")
+    fetch("https://vic.uy/project/")
       .then((response) => response.json())
       .then((data) => {
         projects = data;
